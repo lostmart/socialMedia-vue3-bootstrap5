fe2b0d30e6a96db4f59debaae7b3700571c6a7f5
lostmart <lostmart@hotmail.com>
2022-09-25 20:24:36 +0200

    nuevs partes y componentes

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,41 +1,34 @@
 <template>
-  <div class="card shadow rounded">
-    <img v-if="imgUrl" :src="imgUrl" class="card-img-top" alt="..." />
-    <div class="card-body">
-      <h5 v-if="title" class="card-title">{{ title }}</h5>
-      <p v-if="cardParragrapho" class="card-text">
-        {{ cardParragrapho }}
-      </p>
-      <slot></slot>
-      <div class="d-grid gap-2">
-        <button
-          v-for="btn in btns"
-          :key="btn.texto"
-          class="btn"
-          :class="btn.class"
-        >
-          {{ btn.text }}
-        </button>
-      </div>
-    </div>
-  </div>
+	<div class="card">
+		<div class="card-body">
+			<h5 v-if="title" class="card-title">{{ title }}</h5>
+			<p v-if="parrafo" class="card-text">
+				{{ parrafo }}
+			</p>
+			<slot></slot>
+			<div class="d-grid gap-2">
+				<button
+					v-for="btn in btns"
+					:key="btn.txt"
+					class="btn"
+					:class="btn.class">
+					{{ btn.txt }}
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-  export default {
-    name: "CardComp",
-    props: {
-      imgUrl: String,
-      title: String,
-      cardParragrapho: String,
-      btns: Array
-    }
-
-  }
+	export default {
+		name: 'CardComp',
+		props: ['title', 'parrafo', 'btns'],
+	}
 </script>
 
 <style scoped>
-.card {
-  border: none;
-}
+	.card {
+		border: none;
+		box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
+	}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,54 @@
 <template>
 	<div class="home">
 		<div class="row">
-			<div class="col">
-				<img alt="Vue logo" src="../assets/img/wordmark.svg" />
+			<div class="col-lg-6">
+				<div class="img-cont">
+					<img alt="Vue logo" src="../assets/img/wordmark.svg" />
+					<h2 class="text-center text-lg-start">
+						Conéctate con amigos y el resto del mundo usando Mazebook
+					</h2>
+				</div>
 			</div>
-			<div class="col"></div>
+			<div class="col-lg-6">
+				<card-comp :btns="btnArray" class="mx-auto card-comp">
+					<form>
+						<div class="mb-3 mt-2">
+							<input
+								type="email"
+								class="form-control"
+								placeholder="Ingresa tu email" />
+						</div>
+						<div class="mb-3 mt-2">
+							<input
+								type="password"
+								class="form-control"
+								placeholder="Contraseña" />
+						</div>
+					</form>
+				</card-comp>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-	import CardComp from '@/components/CardComp.vue'
 	// @ is an alias to /src
-
+	import CardComp from '../components/CardComp.vue'
 	export default {
 		name: 'HomeView',
-		components: { CardComp },
+		components: {
+			CardComp,
+		},
+
 		data() {
 			return {
-				imgUrl: '',
-				title: '',
-				cardParragrapho: '',
 				btnArray: [
 					{
-						text: 'hola boton',
-						class: 'btn-success',
+						txt: 'Iniciar sesión',
+						class: 'btn-primary',
 					},
 					{
-						text: 'Borrar',
+						txt: 'Borrar',
 						class: 'btn-danger',
 					},
 				],
@@ -36,4 +57,38 @@
 	}
 </script>
 
-<style scoped></style>
+<style scoped>
+	.home {
+		max-width: 895px;
+		margin: auto;
+	}
+	.img-cont {
+		display: flex;
+		flex-direction: column;
+	}
+
+	img {
+		max-width: 315px;
+		margin: 1em auto 0 auto;
+	}
+
+	.card-comp {
+		max-width: 398px;
+		margin-top: 2.6em;
+	}
+
+	@media (min-width: 992px) {
+		.home {
+			margin: 4em auto;
+		}
+		img {
+			margin: 0;
+			margin-top: 5.6em;
+		}
+		h2 {
+			margin: 0;
+			margin-top: -0.8em;
+			margin-left: 0.8em;
+		}
+	}
+</style>
