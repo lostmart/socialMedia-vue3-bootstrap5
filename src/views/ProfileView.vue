93258ea6d70076fccad2da54d9d25e02143947a8
lostmart <martinpedraza1979@gmail.com>
2025-05-14 19:29:45 +0200

    new

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,32 @@
 <template>
-	<h2>Profile page</h2>
+	<section class="container">
+		<header>
+			<img src="/imgs/forest.jpeg" alt="header image" class="img-fluid">
+		</header>
+		<h1>Perfil</h1>
+	</section>
 </template>
 
 <script>
-	export default {
-		name: 'ProfileView',
-	}
+export default {
+	name: 'ProfileView',
+}
 </script>
+
+
+<style scoped>
+section {
+	min-height: 75dvh;
+}
+
+.container {
+	padding: 0;
+}
+
+header>img {
+	max-width: 1095px;
+	object-fit: cover;
+	max-height: 139px;
+	width: 100%;
+}
+</style>
